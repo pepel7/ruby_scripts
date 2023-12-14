521f8b0229c86697f7a60e6dc14f180e5aa1d6f0
pepel7 <dasha.per.2016@gmail.com>
2023-12-14 16:33:00 +0200

    Refactor stock_picker.rb

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,8 +1,8 @@
 def stock_picker(stock_prices_per_day)
   prices = stock_prices_per_day.map(&:clone)
+  max = prices.max
 
-  loop do
-    max = prices.max
+  loop do # get rid of the maxes at the beginning of the array
     if prices.index(max) == 0
       prices.delete(max)
     end
@@ -10,8 +10,9 @@ def stock_picker(stock_prices_per_day)
     break if prices.index(max) != 0
   end
 
-  result = prices[0..prices.index(prices.max)].minmax
-  result_indexes = result.map { |value| stock_prices_per_day.index(value) } 
+  result = prices[0..prices.index(max)]
+           .minmax
+           .map { |value| stock_prices_per_day.index(value) } 
 end
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
